796236a9b7b294ff99efcc6568b2394e4a970166
Hani <139141380+Darkksoul@users.noreply.github.com>
2025-03-18 22:55:36 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -7,7 +7,7 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import DATABASE_URI2, DATABASE_NAME, COLLECTION_NAME, MAX_BTN
+from info import DATABASE_NAME, COLLECTION_NAME, MAX_BTN
 
 client = AsyncIOMotorClient(DATABASE_URI2)
 mydb = client[DATABASE_NAME]
